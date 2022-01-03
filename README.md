c8f8ab72c0d921623205d51cd24d21e4245df4b0
RETAIL2U\lmer671 <vasanthi.ayyagari@theverygroup.com>
2022-01-03 16:20:01 +0000

    Jenkins-repo: initial commit for the dev ops jenkins

---
+++ README.md
@@ -0,0 +1,5 @@
+## Running Jenkins
+
+`./gradlew docker dockerRun`
+
+Jenkins will then be available at [http://localhost:8080](http://localhost:8080).
---
+++ createJobs.groovy
@@ -0,0 +1,37 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/vasuayyagari/cake-service.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/vasuayyagari/cake-service.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
